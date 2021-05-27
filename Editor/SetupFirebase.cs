2ece3bf445ef160ebec8f93faa2bc4e9eacef276
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-05-27 11:25:33 +0700

    move scripts to one folder. include cloud messaging helper as extra script package

---
+++ Editor/SetupFirebase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Omnilatent.Firebase.Editor
+{
+    public class SetupFirebase
+    {
+        [MenuItem("Tools/Omnilatent/Firebase/Import Cloud MessagingHelper")]
+        public static void ImportCloudMessagingHelper()
+        {
+            string path = GetPackagePath("Assets/Omnilatent/FirebaseManager/FirebaseCloudMessagingHelperPack.unitypackage", "FirebaseCloudMessagingHelperPack");
+            AssetDatabase.ImportPackage(path, true);
+        }
+
+        static string GetPackagePath(string path, string filename)
+        {
+            if (!File.Exists($"{Application.dataPath}/../{path}"))
+            {
+                Debug.Log($"{filename} not found at {path}, attempting to search whole project for {filename}");
+                string[] guids = AssetDatabase.FindAssets($"{filename} l:package");
+                if (guids.Length > 0)
+                {
+                    path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                }
+                else
+                {
+                    Debug.LogError($"{filename} not found at {Application.dataPath}/../{path}");
+                    return null;
+                }
+            }
+            return path;
+        }
+    }
+}
