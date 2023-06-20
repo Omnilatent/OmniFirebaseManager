bc24e3e7b00d636b6a26a128c69ca213bc3ac4da
Trong Nguyen <nguyentrong101094@gmail.com>
2023-06-20 14:25:13 +0700

    Auto add OMNILATENT_FIREBASE_MANAGER to scripting define symbol.

---
+++ Editor/ScriptingDefineSymbolFirebaseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+#if !OMNILATENT_FIREBASE_MANAGER
+namespace Omnilatent.Firebase.Editor
+{
+    public static class ScriptingDefineSymbolAdsManager
+    {
+        const string SYMBOL = "OMNILATENT_FIREBASE_MANAGER";
+
+        [InitializeOnLoadMethod]
+        private static void Init()
+        {
+            // Get current defines
+            string defineSymbolString = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup);
+            // Split at ;
+            List<string> symbols = defineSymbolString.Split(';').ToList();
+            // check if defines already exist given define
+            if (!symbols.Contains(SYMBOL))
+            {
+                // if not add it at the end with a leading ; separator
+                defineSymbolString += $";{SYMBOL}";
+
+                // write the new defines back to the PlayerSettings
+                // This will cause a recompilation of your scripts
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, defineSymbolString);
+
+                Debug.Log($"Scripting Define Symbol '{SYMBOL}' was added.");
+            }
+        }
+    }
+}
+#endif
